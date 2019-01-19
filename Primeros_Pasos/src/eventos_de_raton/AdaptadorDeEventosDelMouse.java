864a2cf9bc19e2f7ada08d213ccbe48c2b7c092d
nan2015 <nanacuna1992@gmail.com>
2019-01-19 18:45:05 -0300

    I made modifications to the file (AdaptadorDeEventosDelMouse)

--- Primeros_Pasos/src/eventos_de_raton/AdaptadorDeEventosDelMouse.java
+++ Primeros_Pasos/src/eventos_de_raton/AdaptadorDeEventosDelMouse.java
@@ -3,6 +3,16 @@ import java.awt.event.*;
 
 public class AdaptadorDeEventosDelMouse extends MouseAdapter {
 	public void mouseClicked (MouseEvent e) {
-		System.out.println("Haz hecho click");
+		/*System.out.println("Coordenada X: "+e.getX()+ 
+						"\nCoordenanda Y: "+e.getY());*/
+		//System.out.println(e.getClickCount());
+	}
+	public void mousePressed (MouseEvent e) {
+		if (e.getModifiersEx()==MouseEvent.BUTTON1_DOWN_MASK)
+			System.out.println("Haz pulsado el botón izquierdo del mouse");
+		else if (e.getModifiersEx()==MouseEvent.BUTTON2_DOWN_MASK)
+			System.out.println("Haz pulsado la rueda del mousse");
+		else if (e.getModifiersEx()==MouseEvent.BUTTON3_DOWN_MASK)
+			System.out.println("Haz pulsado el botón derecho del mouse");
 	}
 }
