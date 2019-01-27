b08082b41086e769629643f0acdf51b5043ba8ab
nan2015 <nanacuna1992@gmail.com>
2019-01-26 21:01:05 -0300

    I added a new file template

--- Primeros_Pasos/src/z_template/Layer.java
+++ Primeros_Pasos/src/z_template/Layer.java
@@ -1,9 +1,11 @@
 package z_template;
+import java.awt.*;
 import javax.swing.*;
-import java.awt.event.*;
+import javax.swing.text.*;
+import javax.swing.event.*;
 
 public class Layer extends JPanel {
-	public Layer () {
-		
-	}
+		public Layer () {
+			
+		}
 }
